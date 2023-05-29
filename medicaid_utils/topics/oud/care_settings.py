628419bb776a30a4277d68d47fc498abb0524cd3
Manu Murugesan <manorathan@uchicago.edu>
2023-05-28 22:40:48 -0500

    feat(oud.care_settings): support flagging care settings in line and base files

--- medicaid_utils/topics/oud/care_settings.py
+++ medicaid_utils/topics/oud/care_settings.py
@@ -25,49 +25,76 @@ def flag_care_settings(df_ot: dd.DataFrame):
     """
     df_ot = df_ot.assign(
         **{
-            "fqhc": (
-                (dd.to_numeric(df_ot["POS_CD"], errors="coerce") == 50)
-                | (df_ot["BLG_PRVDR_TXNMY_CD"].str.strip() == "261QF0400X")
-            ).astype(int),
-            "outpatient_hospital": dd.to_numeric(
-                df_ot["POS_CD"], errors="coerce"
-            )
-            .isin(
-                [
-                    12,
-                    22,
-                ]
-            )
-            .astype(int),
-            "physician_office": (
-                (dd.to_numeric(df_ot["POS_CD"], errors="coerce") == 11)
-                | (
-                    dd.to_numeric(df_ot["TOS_CD"], errors="coerce").isin(
-                        [8, 12, 36, 37]
-                    )
-                )
-            ).astype(int),
-            "behavioral_health_centers": (
-                dd.to_numeric(df_ot["POS_CD"], errors="coerce").isin(
-                    [
-                        52,
-                        53,
-                        55,
-                        56,
-                        57,
-                        58,
-                    ]
-                )
-                | df_ot["BLG_PRVDR_TXNMY_CD"]
-                .str.strip()
-                .isin(["261QM0855X", "261QM0801X", "261QM2800X", "261QR0405X"])
-            ).astype(int),
-            "hospital": dd.to_numeric(df_ot["POS_CD"], errors="coerce")
-            .isin([21, 22, 23])
-            .astype(int),
-            "office_based": dd.to_numeric(df_ot["POS_CD"], errors="coerce")
-            .isin([11, 17, 19, 20, 49, 71, 72])
-            .astype(int),
+            setting: 0
+            for setting in [
+                "fqhc",
+                "outpatient_hospital",
+                "physician_office",
+                "behavioral_health_centers",
+                "hospital",
+                "office_based",
+            ]
         }
     )
+    if ("POS_CD" in df_ot.columns) and ("BLG_PRVDR_TXNMY_CD" in df_ot.columns):
+        df_ot = df_ot.assign(
+            **{
+                "fqhc": (
+                    (dd.to_numeric(df_ot["POS_CD"], errors="coerce") == 50)
+                    | (df_ot["BLG_PRVDR_TXNMY_CD"].str.strip() == "261QF0400X")
+                ).astype(int),
+                "outpatient_hospital": dd.to_numeric(
+                    df_ot["POS_CD"], errors="coerce"
+                )
+                .isin(
+                    [
+                        12,
+                        22,
+                    ]
+                )
+                .astype(int),
+                "physician_office": (
+                    dd.to_numeric(df_ot["POS_CD"], errors="coerce") == 11
+                ).astype(int),
+                "behavioral_health_centers": (
+                    dd.to_numeric(df_ot["POS_CD"], errors="coerce").isin(
+                        [
+                            52,
+                            53,
+                            55,
+                            56,
+                            57,
+                            58,
+                        ]
+                    )
+                    | df_ot["BLG_PRVDR_TXNMY_CD"]
+                    .str.strip()
+                    .isin(
+                        [
+                            "261QM0855X",
+                            "261QM0801X",
+                            "261QM2800X",
+                            "261QR0405X",
+                        ]
+                    )
+                ).astype(int),
+                "hospital": dd.to_numeric(df_ot["POS_CD"], errors="coerce")
+                .isin([21, 22, 23])
+                .astype(int),
+                "office_based": dd.to_numeric(df_ot["POS_CD"], errors="coerce")
+                .isin([11, 17, 19, 20, 49, 71, 72])
+                .astype(int),
+            }
+        )
+    if "TOS_CD" in df_ot.columns:
+        df_ot = df_ot.assign(
+            **{
+                "physician_office": dd.to_numeric(
+                    df_ot["TOS_CD"], errors="coerce"
+                )
+                .isin([8, 12, 36, 37])
+                .astype(int),
+            }
+        )
+
     return df_ot
