17221acd47c3660e16a88f738fc4935eeb81a88b
Manu Murugesan <manorathan@uchicago.edu>
2024-10-26 12:35:14 -0500

    fix(max): add temporary workaround for unsorted 2015 MAX files

--- medicaid_utils/filters/patients/cohort_extraction.py
+++ medicaid_utils/filters/patients/cohort_extraction.py
@@ -422,7 +422,7 @@ def extract_cohort(  # pylint: disable=too-many-locals, missing-param-doc
                                 pq_engine=pq_engine,
                                 **(
                                     {}
-                                    if claim_type == "ip"
+                                    if ((claim_type == "ip") or (year == 2015))
                                     else {
                                         "tmp_folder": os.path.join(
                                             dct_data_paths["tmp_folder"],
